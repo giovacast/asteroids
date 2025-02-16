bdadd75683b52ab2561403f9bae51632dd92ec60
giovacast <giovacastiglione@gmail.com>
2025-02-16 14:10:57 -0400

    updated main.py: added clock object abd used tick() method to set fps at 60

--- main.py
+++ main.py
@@ -5,13 +5,22 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    
+
+        clock.tick(60)
+
+        dt = clock.tick(60) / 1000.0
+
+
+
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
