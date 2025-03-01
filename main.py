dfd69ef6d8d43e6bcb1ef1d319b43486668051f7
giovacast <giovacastiglione@gmail.com>
2025-03-01 11:29:45 -0400

    updated the main.py file after adding a player instance

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+import circleshape
+import player
 
 def main():
     pygame.init()
@@ -8,8 +10,11 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    p1 = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         pygame.Surface.fill(screen, (0, 0, 0))
+        p1.draw(screen)    
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -19,12 +24,9 @@ def main():
 
         dt = clock.tick(60) / 1000.0
 
-
-
-
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+print("Starting asteroids!")
+print(f"Screen width: {SCREEN_WIDTH}")
+print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
